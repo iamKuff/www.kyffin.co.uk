1280c313e5860ab3a4876effe69384ceb7e97004
ItsKuf <104674648+ItsKuf@users.noreply.github.com>
2024-07-01 13:24:36 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const icons = document.querySelectorAll('.construction-icons img');
-  icons.forEach((icon, index) => {
-    setTimeout(() => {
-      icon.style.animation = `fadeIn 2s ease-in-out ${index * 0.5}s forwards`;
-    }, index * 500);
-  });
-});
